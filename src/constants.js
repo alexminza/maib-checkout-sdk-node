3848df9aea1b3c1d723cd74accb6cac7dd3ed4ce
Alexander Minza <alexander.minza@gmail.com>
2026-01-04 21:48:59 +0200

    MaibCheckoutApiRequest

--- src/constants.js
+++ src/constants.js
@@ -20,15 +20,19 @@ const API_ENDPOINTS = {
     CHECKOUTS_DETAILS: 'checkouts/:checkoutId',
 
     PAYMENTS: 'payments',
-    PAYMENTS_ID: 'payments/:payId',
-    PAYMENTS_REFUND: 'payments/:payId/refund',
+    PAYMENTS_ID: 'payments/:paymentId',
+    PAYMENTS_REFUND: 'payments/:paymentId/refund',
+
+    MIA_TEST_PAY: 'mia/test-pay'
 };
 
 const REQUIRED_PARAMS = {
     // https://docs.maibmerchants.md/checkout/api-reference/endpoints/register-a-new-hosted-checkout-session#request
     CHECKOUT_PARAMS: ['amount', 'currency', 'orderInfo', 'callbackUrl'],
     // https://docs.maibmerchants.md/checkout/api-reference/endpoints/refund-a-payment#request
-    PAYMENTS_REFUND_PARAMS: ['amount', 'reason']
+    PAYMENTS_REFUND_PARAMS: ['amount', 'reason'],
+    // https://docs.maibmerchants.md/mia-qr-api/en/payment-simulation-sandbox#request-parameters-body-json
+    MIA_TEST_PAY_PARAMS: ['qrId', 'amount', 'iban', 'currency', 'payerName'],
 }
 
 module.exports = {
